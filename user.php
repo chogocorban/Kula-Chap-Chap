0da697eaddcf82d16c8194343260945d2abc8e5b
chogo123 <corban.chogo@strathmore.edu>
2023-11-17 19:20:45 +0300

    .

---
+++ user.php
@@ -0,0 +1,61 @@
+<?php 
+//session_start();
+
+	include 'connection.php';
+	
+	//$user_data = check_login($conn);
+
+	if (isset($_POST['submit'])) {
+		// code...
+		$name=$_POST['name'] ;
+		$email=$_POST['email']; 
+		$password= $_POST['password'];
+
+		$sql= "INSERT INTO users2(name,email,password) VALUES('$name','$email','$password')"
+		$result=mysqli_query($conn,$sql);
+		if ($result) {
+			// code...
+			echo "Data inserted succesfully";
+		}else{
+			die(mysqli_error($conn))	
+		}
+	}
+ ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+<div class="content">
+	
+		<form method="post">
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+  	</div>
+
+  	  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+  	</div>
+ 
+ 	 <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" placeholder="Enter your password" name="password" autocomplete="off">
+  	</div>
+
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+
+
+</div>
+
+</body>
+</html>
